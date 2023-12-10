5a50064420c7d9a834b04e6d0bb4d61b9f5d9499
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2023-12-10 16:26:30 -0500

    first commit

---
+++ build/CMakeFiles/NotepadC.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NotepadC.dir/src/main.c.obj"
+  "CMakeFiles/NotepadC.dir/src/main.c.obj.d"
+  "NotepadC.exe"
+  "NotepadC.exe.manifest"
+  "NotepadC.pdb"
+  "libNotepadC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NotepadC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MONTH_OFFSET 1;
+#define YEAR_OFFSET 1900;
+
+typedef struct User
+{
+    char *username;
+    unsigned short int age;
+} User;
+
+typedef struct Date
+{
+    struct tm *current_date;
+    struct tm *current_time;
+} Date;
+
+typedef struct Document
+{
+    User *author;
+    Date *created_at;
+    Date *updated_at;
+    char *subject;
+    char *contents;
+} Document;
+
+Date *get_current_date_time()
+{
+    time_t t = time(NULL);
+    Date *new_date = malloc(sizeof(struct tm) * 2);
+    new_date->current_date = localtime(&t);
+    new_date->current_time = localtime(&t);
+    new_date->current_date->tm_mon = new_date->current_date->tm_mon + MONTH_OFFSET;
+    new_date->current_date->tm_year = new_date->current_date->tm_year + YEAR_OFFSET;
+
+    return new_date;
+}
+
+int main()
+{
+    printf("HELLO WORLD");
+    Date *current_date_time = get_current_date_time();
+
+    printf("%d", current_date_time->current_date->tm_year);
+    return 0;
+}
